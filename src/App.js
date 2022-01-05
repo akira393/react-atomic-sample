ed84e81084fbd1066ba8ee8e3ff36158c727bd66
akira <40556969+akira393@users.noreply.github.com>
2022-01-05 13:52:01 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PrimaryButton>click</PrimaryButton>
+      <br />
+      <br />
+      <br />
+      <SecondaryButton>検索</SecondaryButton>
+    </div>
+  );
+}
